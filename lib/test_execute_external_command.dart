afc77215561a02cb6ed7aa5721ae68ebcbfea77e
Hank <y47106@gmail.com>
2022-05-30 00:09:38 +0800

    add execute external command

---
+++ lib/test_execute_external_command.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main(List<String> args) async {
+  var executable = 'ls';
+  if (Platform.isWindows) {
+    executable = 'dir';
+  }
+
+  final arguments = <String>[];
+  print('List Files and Directories');
+  print('============');
+  final process = await Process.start(executable, arguments, runInShell: true);
+  await stdout.addStream(process.stdout);
+  await stderr.addStream(process.stderr);
+  final exitCode = await process.exitCode;
+  print('============');
+  print('Exit code: $exitCode');
+}
